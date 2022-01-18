bf85c8d02385369ce3695fec6c07a177f878ee18
Mark Laptenok <mark@codelearning.online>
2022-01-18 22:47:15 +0300

    Modified Info, Get_function_name, get_function_name functions

--- logger.go
+++ logger.go
@@ -1,5 +1,7 @@
 package logger
 
+//	TO-DO:	use the 'log' package to format (add timestamp) and output string to syslog and stderr.
+
 import (
 	"fmt"
 	"log"
@@ -19,13 +21,18 @@ func (e *ClpError) Error() string {
 
 //	Exported functions
 
+//	Checks whetner logger works (can write to the log sink: syslog, stderr) or not.
 func Check() error {
 	//	TO-DO: replace with the actual activity
-	fmt.Println("Check is activated")
 
+	/*
+		location, _ := Get_function_name()
+		return &ClpError{1, "Check", location}
+	*/
 	return nil
 }
 
+//	TO-DO:	think about making interface like the 'Info' function has.
 func Debug(err error) error {
 	//	TO-DO: replace with the actual activity
 	fmt.Printf("[DEBUG] %s\n", err)
@@ -40,47 +47,52 @@ func Error(err error) error {
 	return nil
 }
 
-func Info(msg string) error {
+func Info(format string, args ...interface{}) error {
 	//	TO-DO: replace with the actual activity
-	fmt.Printf("[INFO] %s: %s\n", get_function_name(3), msg)
+	data := fmt.Sprintf(format, args...)
+	if location, func_err := get_function_name(3); func_err == nil {
+		fmt.Printf("[INFO] %s: %s\n", location, data)
+	} else {
+		//	TO-DO: Add cheking of debug flag
+		Debug(func_err)
+		fmt.Printf("[INFO] %s\n", data)
+	}
 
 	return nil
 }
 
 //	Returns name of function which calls this one.
-func Get_function_name() string {
-
-	defer ""
+func Get_function_name() (string, error) {
 
 	program_counters := make([]uintptr, 1)
 	amount_of_functions_in_the_callstack := runtime.Callers(2, program_counters)
 	if amount_of_functions_in_the_callstack == 0 {
-		return 
+		return "", &ClpError{2, "There are no program counters", "Get_function_name()"}
 	}
+
 	frames := runtime.CallersFrames(program_counters[:amount_of_functions_in_the_callstack])
 	frame, _ := frames.Next()
 
-	return frame.Function
+	return frame.Function, nil
 }
 
-//	Returns name of function which calls this one.
-func get_function_name(depth int) string {
+//	Returns name of function which calls this one or an error instead.
+func get_function_name(depth int) (string, error) {
 
-	defer ""
-
-	if depth == 0 {
-		depth = 2
+	if depth <= 0 {
+		return "", &ClpError{1, "Depth should be greater than 0", "get_function_name(depth int)"}
 	}
 
 	program_counters := make([]uintptr, 1)
 	amount_of_functions_in_the_callstack := runtime.Callers(depth, program_counters)
 	if amount_of_functions_in_the_callstack == 0 {
-		return 
+		return "", &ClpError{2, "There are no program counters. Check value of the depth argument", "get_function_name(depth int)"}
 	}
+
 	frames := runtime.CallersFrames(program_counters[:amount_of_functions_in_the_callstack])
 	frame, _ := frames.Next()
 
-	return frame.Function
+	return frame.Function, nil
 }
 
 //	Static functions
@@ -88,7 +100,13 @@ func get_function_name(depth int) string {
 func to_syslog(err error) error {
 
 	//	TO-DO: replace with the actual activity
-	fmt.Printf("%s: %s\n", get_function_name(3), err)
+	if location, func_err := get_function_name(3); func_err == nil {
+		fmt.Printf("%s: %s\n", location, err)
+	} else {
+		//	TO-DO: Add checking of debug flag
+		Debug(func_err)
+		fmt.Printf("%s\n", err)
+	}
 
 	return nil
 }
@@ -96,7 +114,13 @@ func to_syslog(err error) error {
 func to_stderr(err error) error {
 
 	//	Prints to stderr.
-	log.Println("%s: %s\n", get_function_name(3), err)
+	if location, func_err := get_function_name(3); func_err == nil {
+		log.Printf("%s: %s\n", location, err)
+	} else {
+		//	TO-DO: Add checking of debug flag
+		Debug(func_err)
+		log.Printf("%s\n", err)
+	}
 
 	return nil
 }
